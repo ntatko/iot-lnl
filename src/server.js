fe713198bf692ac13643526d66caad75d2ea8d8f
Noah Tatko <tatkon@rbtrainers.com>
2022-10-01 17:12:55 -0500

    reset server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,8 @@
-const { Board, Button, Servo } = require("johnny-five");
+const { Board } = require("johnny-five");
 const board = new Board();
 
 board.on("ready", () => {
-    const button = new Button(3);
-    const servo = new Servo(4);
-    const positions = [0, 45, 90, 135, 180];
-    let positionIndex = 0;
-
-    button.on("press", () => {
-        positionIndex = (positionIndex + 1) % positions.length;
-        servo.to(positions[positionIndex]);
-    });
+   // Your code here!
    
 }).on("error", err => {
     console.log(err);
